ba2e3715c821dd0693802c4573965a81731049c1
lorena-ipsum <lorenanbo@gmail.com>
2024-02-18 12:41:32 +0100

    video en background - faut corriger le fade in des sections

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -1,11 +1,21 @@
 <?php
-// Enqueu parent et child styles 
+// Enqueue parent et child styles 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    // Enfileira o estilo do tema pai
+    // Enqueue parent style
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
-    // Enfileira o estilo do tema filho (style.css)
+    // Enqueue child style (style.css)
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
 }
+
+
+//Enqueue scripts.js
+function enqueue_custom_scripts() {
+    wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/script.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
+
+
+
 ?>

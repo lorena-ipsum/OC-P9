6a307bf2f261ff7fdcb88b90995732af53290b7e
lorena-ipsum <lorenanbo@gmail.com>
2024-02-20 11:18:36 +0100

    video ok, cest parti pour mettre les effets de page in lors du scroll de la page

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -1,74 +1,78 @@
 <?php
+
 get_header();
 ?>
 
-<main id="primary" class="site-main">
+    <main id="primary" class="site-main">
     <section class="banner">
-        <!-- Configuration de la vidéo de fond en boucle, avec une image en poster qui sera afficher pendant le chargement de la video -->
-        <div class="video-background">
-            <video autoplay loop muted>
-                <source src="/P9/wp-content/themes/foce-child/assets/videos/Studio_Koukaki-vidéo-header-sans-son.mp4" poster="/P9/wp-content/themes/foce-child/assets/images/banner.png" type="video/mp4">
-                <!-- Ajoutez d'autres formats vidéo ici (WebM, Ogg, etc.) pour une compatibilité maximale -->
-            </video>
-        </div>
-        <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?>" alt="logo Fleurs d'oranger & chats errants">
-    </section>
-    <section id="story" class="story fade-in-section">
-        <h2>L'histoire</h2>
-        <article id="" class="story__article">
-            <p><?php echo get_theme_mod('story'); ?></p>
-        </article>
-        <?php
-        $args = array(
-            'post_type' => 'characters',
-            'posts_per_page' => -1,
-            'meta_key'  => '_main_char_field',
-            'orderby'   => 'meta_value_num',
-        );
-        $characters_query = new WP_Query($args);
-        ?>
-        <article id="characters" class="fade-in-section">
-            <div class="main-character">
-                <h3>Les personnages</h3>
-                <?php
-                $main_character = $characters_query->posts[0];
-                echo '<figure>';
-                echo get_the_post_thumbnail($main_character->ID, 'full');
-                echo '<figcaption>' . $main_character->post_title . '</figcaption>';
-                echo '</figure>';
-                $characters_query->next_post();
-                ?>
-            </div>
-            <div class="other-characters">
-                <?php
-                while ($characters_query->have_posts()) {
-                    $characters_query->the_post();
+       <!-- Configuration de la vidéo de fond en boucle, avec une image en poster qui sera afficher pendant le chargement de la video -->
+       <div class="video-background">
+           <video autoplay loop muted>
+               <source src="/P9/wp-content/themes/foce-child/assets/videos/Studio_Koukaki-vidéo-header-sans-son.mp4" poster="/P9/wp-content/themes/foce-child/assets/images/banner.png" type="video/mp4">
+               <!-- Ajoutez d'autres formats vidéo ici (WebM, Ogg, etc.) pour une compatibilité maximale -->
+           </video>
+       </div>
+       <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?>" alt="logo Fleurs d'oranger & chats errants">
+   </section>
+        <section id="#story" class="story">
+            <h2>L'histoire</h2>
+            <article id="" class="story__article">
+                <p><?php echo get_theme_mod('story'); ?></p>
+            </article>
+            <?php
+            $args = array(
+                'post_type' => 'characters',
+                'posts_per_page' => -1,
+                'meta_key'  => '_main_char_field',
+                'orderby'   => 'meta_value_num',
+
+            );
+            $characters_query = new WP_Query($args);
+            ?>
+            <article id="characters">
+                <div class="main-character">
+                    <h3>Les personnages</h3>
+                    <?php
+                    $main_character = $characters_query->posts[0];
                     echo '<figure>';
-                    echo get_the_post_thumbnail(get_the_ID(), 'full');
-                    echo '<figcaption>';
-                    the_title();
-                    echo'</figcaption>';
+                    echo get_the_post_thumbnail( $main_character->ID, 'full' );
+                    echo '<figcaption>'. $main_character->post_title . '</figcaption>';
                     echo '</figure>';
-                }
-                ?>
-            </div>
-        </article>
-        <article id="place" class="fade-in-section">
+                    $characters_query->next_post();
+                    ?>
+                </div>
+                <div class="other-characters">
+                    <?php
+                    while ( $characters_query->have_posts() ) {
+                        $characters_query->the_post();
+                        echo '<figure>';
+                        echo get_the_post_thumbnail( get_the_ID(), 'full' );
+                        echo '<figcaption>';
+                        the_title();
+                        echo'</figcaption>';
+                        echo '</figure>';
+                    }
+                    ?>
+                </div>
+            </article>
+            <article id="place">
+                <div>
+                    <h3>Le Lieu</h3>
+                    <p><?php echo get_theme_mod('place'); ?></p>
+                </div>
+
+            </article>
+        </section>
+
+
+        <section id="studio">
+            <h2>Studio Koukaki</h2>
             <div>
-                <h3>Le Lieu</h3>
-                <p><?php echo get_theme_mod('place'); ?></p>
+                <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
+                <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
             </div>
-        </article>
-    </section>
-    <section id="studio" class="fade-in-section">
-        <h2>Studio Koukaki</h2>
-        <div>
-            <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
-            <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
-        </div>
-    </section>
-</main>
+            </section>
+    </main><!-- #main -->
 
 <?php
 get_footer();
-?>
